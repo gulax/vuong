fde7bc8aaf72d5ce10d27064a1778283faf1ff1c
Phạm Vương <joney.vuong@gmail.com>
2021-10-01 22:01:16 +0700

    cac

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, sum;
+    cout <<"Enter two numbers : "<<endl;
+    cin >> n1 >> n2;
+    sum = n1+n2;
+    cout<< n1 <<" + " << n2 << " = " << sum;
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World!!!"<<endl;
+    cout<<"Hello again!";
+    return 0;
+}
